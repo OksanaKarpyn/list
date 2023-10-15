cd4badb33a185e1c274604f985ceb975fe354739
Oksana Karpyn <oksana101992@gmail.com>
2023-10-15 15:21:35 +0200

    add corezione localstorage

--- script.js
+++ script.js
@@ -1,72 +1,4 @@
-// let makeList = document.querySelector('.list-make');
-// let listDone = document.querySelector('.list-done')
-// let i1 = document.querySelector('#input-list');
-// let btnForm = document.querySelector('.btn-form');
 
-// //------function-----
-// function createMakeListItem(){
-//     let inputValue = i1.value.trim();
-    
-//     let ul = document.createElement('ul');
-//     makeList.append(ul);
-//     let li = document.createElement('li');
-//     li.classList.add('li-make');
-//     ul.appendChild(li);
-//     let div = document.createElement('div');
-//     li.appendChild(div);
-//     let input = document.createElement('input');
-//     input.type='checkbox';
-//     div.appendChild(input);
-//     let span = document.createElement('span');
-//     span.textContent = `${inputValue}`;
-//     div.appendChild(span);
-//     let button = document.createElement('button');
-//     button.classList.add('btn','btn-light')
-//    // button.textContent = '✔fatto';
-//     li.appendChild(button);
-
-//         input.addEventListener('change', function(){
-//             if (this.checked) {
-//                 span.style.textDecoration = 'line-through';
-//                 button.textContent = '✔fatto';
-//                 button.addEventListener('click',function(){
-//                     ul.removeChild(li)
-//                     addToDoneList(inputValue);
-//                 })
-//             } else {
-//                 span.style.textDecoration = 'none';
-//                 button.textContent = ' ';
-//             }
-//         })
-//         inputValue = '';
-// }
-// function  addToDoneList(inputValue){
-//     let newul = document.createElement('ul');
-//     listDone.appendChild(newul);
-//     let newli = document.createElement('li');
-//     newli.classList.add('li-done');
-//     newul.appendChild(newli);
-//     let p = document.createElement('p');
-//     p.classList.add('lip');
-//     p.textContent = `${inputValue}`;
-//     newli.append(p);
-//     let newbutton = document.createElement('button');
-//     newbutton.classList.add('btn','btn-light')
-//     newbutton.textContent = '❌delete';
-//     newli.append(newbutton);
-//     newbutton.addEventListener('click', function(){
-//       newul.removeChild(newli);
-//    })
-
-// }
-// function f1(event){
-//     event.preventDefault();
-//     let inputValue = i1.value.trim();
-//     if (inputValue !== '') {
-//         createMakeListItem(inputValue);
-//     }
-// }
-// btnForm.addEventListener('click',f1);
 
 
 
@@ -75,45 +7,30 @@ let makeList = document.querySelector('.list-make');
 let listDone = document.querySelector('.list-done')
 let i1 = document.querySelector('#input-list');
 let btnForm = document.querySelector('.btn-form');
+//localstorage 
+let savedListDone  = JSON.parse(localStorage.getItem('todoListDone')) || '';
 
-// function loadFromLocalStorage() {
-//     let storedValues = localStorage.getItem('todoList') || '[]';
-//     let todoList = JSON.parse(storedValues);
-//     for (let value of todoList) {
-//        createMakeListItem(value);
-//         addToDoneList(value);
-//         // if (value) {
-//         //     createMakeListItem(value);
-//         // } else {
-//         //     addToDoneList(value);
-//         // }
-//     }
-// }
-function loadFromLocalStorage() {
-    let storedValues = localStorage.getItem('todoList') || '[]';
-    let todoList = JSON.parse(storedValues);
-    for (let value of todoList) {
-        if (value) {
-            if (value.startsWith("[DONE]")) {
-                // L'elemento è completato
-                addToDoneList(value.substring(6)); // Rimuovi "[DONE]" dal valore
-            } else {
-                // L'elemento non è completato
-                createMakeListItem(value);
-            }
-        }
-    }
+for(const item of savedListDone){
+    addToDoneList(item)
+}
+function saveToLocalStorageDone() {
+    // Salva i dati attuali nella lista nel localStorage
+    const todoListDone = Array.from( listDone.querySelectorAll('li')).map(li => li.querySelector('p').textContent);
+    localStorage.setItem('todoListDone', JSON.stringify(todoListDone));
 }
 
-function removeFromLocalStorage(value) {
-    let storedValues = localStorage.getItem('todoList') || '[]';
-    let todoList = JSON.parse(storedValues);
-    const index = todoList.indexOf(value);
-    if (index !== -1) {
-        todoList.splice(index, 1);
-        localStorage.setItem('todoList', JSON.stringify(todoList));
-    }
+
+let savedList  = JSON.parse(localStorage.getItem('todoList')) || '';//questo carica dati dal localstorage
+// Carica dati salvati nel localStorage
+for(const item of savedList){
+    createMakeListItem(item);
 }
+function saveToLocalStorage() {
+    // Salva i dati attuali nella lista nel localStorage
+    const todoList = Array.from(makeList.querySelectorAll('li')).map(li => li.querySelector('span').textContent);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
 
 function createMakeListItem(inputValue) {
     let ul = document.createElement('ul');
@@ -139,20 +56,22 @@ function createMakeListItem(inputValue) {
             button.textContent = '✔fatto';
             button.addEventListener('click', function(){
                 ul.removeChild(li);
-               // addToDoneList(inputValue);
-                addToDoneList(`[DONE] ${inputValue}`);
-                //removeFromLocalStorage(inputValue);
-               
+              addToDoneList(inputValue);
+            
+                 saveToLocalStorage();
+             
             });
         } else {
             span.style.textDecoration = 'none';
             button.textContent = ' ';
         }
+        saveToLocalStorage();
     });
 
     // Pulisci l'input dopo l'aggiunta dell'elemento
     i1.value = '';
-    saveToLocalStorage(inputValue);
+    saveToLocalStorage();
+    // saveToLocalStorage(inputValue);
     //removeFromLocalStorage(inputValue);
   
 }
@@ -165,7 +84,9 @@ function addToDoneList(inputValue) {
     newul.appendChild(newli);
     let p = document.createElement('p');
     p.classList.add('lip');
+
     p.textContent = inputValue;
+  
     newli.append(p);
     let newbutton = document.createElement('button');
     newbutton.classList.add('btn', 'btn-light')
@@ -173,26 +94,13 @@ function addToDoneList(inputValue) {
     newli.append(newbutton);
     newbutton.addEventListener('click', function(){
         newul.removeChild(newli);
-       removeFromLocalStorage(inputValue);
-        //saveToLocalStorage();
+        
+        saveToLocalStorageDone(); 
     });
-    saveToLocalStorage();
-    //removeFromLocalStorage(inputValue);
+    saveToLocalStorageDone() 
 }
-// Funzione per salvare il valore nella localStorage
 
 
-function saveToLocalStorage() {
-    try{
-        let doneItems = Array.from(document.querySelectorAll('.li-done p')).map(item => item.textContent);
-        let makeItems = Array.from(document.querySelectorAll('.li-make span')).map(item => item.textContent);
-        let allItems = makeItems.concat(doneItems);
-        localStorage.setItem('todoList', JSON.stringify(allItems));
-    } catch(error){
-        console.error('Errore nel salvataggio dei dati nella localStorage:', error);
-    }
-   
-}
 
 function f1(event) {
     event.preventDefault();
@@ -208,7 +116,7 @@ btnForm.addEventListener('click', f1);
 
 
 // Carica i dati dalla localStorage all'avvio dell'app
-loadFromLocalStorage();
+//loadFromLocalStorage();
 //localStorage.clear();
 
 
